fd785e2c4cc26f5d7ae6fafb760db118dee526e8
David Sáez Rodríguez <david.saez.rodriguez7@gmail.com>
2022-06-04 21:42:53 +0200

    Creacion de componentes iniciales y borrado de .ts para test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { InstalacionesComponent } from './instalaciones/instalaciones.component';
+import { MetodologiaComponent } from './metodologia/metodologia.component';
+import { UbicacionComponent } from './ubicacion/ubicacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'instalaciones', component: InstalacionesComponent },
+  { path: 'metodologia', component: MetodologiaComponent },
+  { path: 'ubicacion', component: UbicacionComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { InstalacionesComponent } from './instalaciones/instalaciones.component';
+import { MetodologiaComponent } from './metodologia/metodologia.component';
+import { UbicacionComponent } from './ubicacion/ubicacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    InicioComponent,
+    InstalacionesComponent,
+    MetodologiaComponent,
+    UbicacionComponent
   ],
   imports: [
     BrowserModule,
